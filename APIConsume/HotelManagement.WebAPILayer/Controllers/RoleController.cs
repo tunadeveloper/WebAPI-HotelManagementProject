4d3f572dd6f6a1ec2b96644f0fbb9149b1aea6fb
tunadeveloper <tunabusiness25@gmail.com>
2026-02-22 16:27:33 +0300

    Updated

---
+++ APIConsume/HotelManagement.WebAPILayer/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HotelManagement.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.WebAPILayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRole()
+        {
+            var list = await _roleManager.Roles.ToListAsync();
+            return Ok(list);
+        }
+    }
+}
